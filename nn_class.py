fd01eee99bcd9ee0512a7d8816def69aaf14371f
Priyam Tejaswin <priyamtejaswin@gmail.com>
2015-10-10 09:20:15 +0530

    Added a class for QuadraticError and docstrings.
    Lost of changes.
    Added classes for QuadraticError, removed the sigmoid and sigmoid_deriv
    from the NeuralNetwork class - made them global.
    All future error metrics will be classes and non-linearity functions
    will be global functions.
    
     - and yes , i added docstrings.

--- nn_class.py
+++ nn_class.py
@@ -3,12 +3,34 @@ import sys
 from pprint import pprint
 
 def onehot(x, n=10):
+	"""Returns target values in onehot format."""
 	z = np.zeros((x.shape[0], n))
 	for row,val in zip(z,x):
 		row[val] = 1.
 	return z
 
+def sigmoid(x):
+	"""Sigmoid activation."""
+	return 1.0/(1.0+np.exp(-x))
+
+def sigmoid_deriv(x):
+	"""Sigmoid activation derivative."""
+	return sigmoid(x) * (1-sigmoid(x))
+
+class QuadraticError(object):
+	"""Creates a quadratic error object - i will try to model all error metrics as classes.
+	This will allow expansion for better stuff like log-likelihood and cross-entropy.
+	Contains a object.value function and object.deriv function."""
+	def value(self, a, y):
+		"""QuadraticError cost value."""
+		return 0.5*np.linalg.norm(a-y)**2
+
+	def deriv(self, z, a, y):
+		"""QuadraticError cost derivative."""
+		return (a-y) * sigmoid_deriv(z)
+
 class NeuralNetwork(object):
+	"""Creates a artificial neural network object."""
 	def __init__(self, layers, batch_size=2):
 		self.batch_size = batch_size
 		self.layers = layers
@@ -25,41 +47,41 @@ class NeuralNetwork(object):
 				]
 		pprint(table)
 
-	def cost_deriv(self, output_activation, y):
-		return (output_activation - y)
-
-	def sigmoid(self, x):
-		return np.array(1./(1. + np.exp(-x)))
-
-	def sigmoid_deriv(self, x):
-		return self.sigmoid(x) * (1 - self.sigmoid(x))
-
 	def feedforward(self, a):
+		"""Feed forward output - also used in accuracy evaluation."""
 		activations, weighted_inputs = [a], []
 		for w, b in zip(self.weights, self.biases):
 			z = np.dot(w, a) + b
-			a = self.sigmoid(z)
+			a = sigmoid(z)
 			activations.append(a)
 			weighted_inputs.append(a)
 		return activations, weighted_inputs
 
-	def backprop(self, output_activation, weighted_inputs, target):
-		del_out = self.cost_deriv(output_activation, target) * self.sigmoid_deriv(weighted_inputs[-1])
+	def backprop(self, output_activation, weighted_inputs, target, cost):
+		"""Backpropagates the error from the output layer to al hidden layers.""" 
+		del_out = cost.deriv(weighted_inputs[-1], output_activation, target)
 		del_array = [del_out]
 		for w, z in zip(self.weights[::-1], weighted_inputs[-2::-1]):
-			del_array.append(np.dot(w.T, del_array[-1]) * self.sigmoid_deriv(z))
+			del_array.append(np.dot(w.T, del_array[-1]) * sigmoid_deriv(z))
 		return del_array
 
-	def sgd(self, trX, trY, teX=None, teY=None, epochs=500, alpha=0.05):
+	def update_params(self, del_array, activations):
+		"""Udates the model parameters(w,b)."""
+		for w,b,del_val,a in zip(self.weights[::-1], self.biases[::-1], del_array, activations[-2::-1]):
+			w -= self.alpha * np.dot(del_val, a.T)
+			b -= self.alpha * del_val
+
+	def sgd(self, trX, trY, teX=None, teY=None, epochs=500, alpha=0.05, cost=QuadraticError()):
+		"""Performs stochastic gradient descent for a given bactch size.
+		This method also takes a cost object for calculating cost and cost derivative."""
+		self.epochs, self.alpha = epochs, alpha
 		print "\ntraining...\n"
 		mi_trX = trX.shape[0] - trX.shape[0]%self.batch_size
 		for _i_ in xrange(epochs+1):
 			for i,j in zip(range(0, mi_trX, self.batch_size), range(self.batch_size, mi_trX+1, self.batch_size)):
 				activations, weighted_inputs = self.feedforward(trX[i:j, :].T)
-				del_array = self.backprop(activations[-1], weighted_inputs, trY[:, i:j])
-				for w,b,del_val,a in zip(self.weights[::-1], self.biases[::-1], del_array, activations[-2::-1]):
-					w -= alpha * np.dot(del_val, a.T)
-					b -= alpha * del_val
+				del_array = self.backprop(activations[-1], weighted_inputs, trY[:, i:j], cost)
+				self.update_params(del_array, activations)
 
 			if _i_%100==0:
 				if teX.all():
@@ -73,6 +95,7 @@ class NeuralNetwork(object):
 					raw_input('---')
 
 	def evaluate(self, teX, teY):
+		"""Uses the feedforward method to calculate model accuracy on testing data."""
 		mi_teX = teX.shape[0] - teX.shape[0]%self.batch_size
 		means_array = []
 		for i,j in zip(range(0, mi_teX, self.batch_size), range(self.batch_size, mi_teX+1, self.batch_size)):
